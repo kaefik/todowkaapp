38a5aa063f49dc5aa8a83b4cd9a86bb0c79ac8c0
ilnursoft <ilnursoft@gmail.com>
2026-04-10 09:59:32 +0300

    chore: update dev service worker revision

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2ks4ak6umeg"
+    "revision": "0.n6fca45v4p8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
