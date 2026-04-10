9b8f503c96a834430a52fed0ceb2fca9b1533d8f
ilnursoft <ilnursoft@gmail.com>
2026-04-10 22:22:20 +0300

    chore: добавлены задачи в бэклог — контекст в карточке задачи и color picker для контекстов

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sg2hcjq652"
+    "revision": "0.6hf17b8htgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
